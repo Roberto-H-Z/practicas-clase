943588a926ac1a5a0cad07d42f7c85929d0921c8
Roberto-H-Z <20233l001058@gmail.com>
2025-01-20 13:06:27 -0600

    formulario

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+
+  email: string = '';
+  password: string = '';
+
+
+  constructor(
+    private toastController: ToastController,
+    private navController: NavController,
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+  if (this.email === 'herrerabeto501gmail.com' && this.password === 'beto1234') {
+    // Autenticación exitosa, navega a la pantalla de inicio
+    this.navController.navigateRoot('/inicio');
+  } else {
+    // Autenticación fallida, muestra un mensaje de error
+    alert('Credenciales incorrectas');
+    
+  }   
+}
+
+  async presentToast(position: 'top' | 'bottom' | 'middle') {
+    const toast = await this.toastController.create({
+      message: 'Credenciales incorrectas!',
+      duration: 1500,
+      color: 'danger',
+      position: position
+    });
+    await toast.present();
   }
 
 }
